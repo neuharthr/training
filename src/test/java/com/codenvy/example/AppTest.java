24e156e5c0cfac6f6d73f6be5157a7b62194c8b1
Ryan Neuharth <neuharthr@gmail.com>
2021-04-17 18:45:28 -0500

    Test

--- src/test/java/com/codenvy/example/AppTest.java
+++ src/test/java/com/codenvy/example/AppTest.java
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *  Just a test
  *--------------------------------------------------------------------------------------------*/
 package com.codenvy.example;
 
