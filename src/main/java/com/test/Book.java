36bddd53cd59818c51964a72d644e149f0538ee7
Ryan Neuharth <neuharthr@gmail.com>
2021-04-17 20:46:34 -0500

    Book name update

--- src/main/java/com/test/Book.java
+++ src/main/java/com/test/Book.java
@@ -32,8 +32,8 @@ public class Book {
 
     public static void main(String[] arguments) {
         // Small test of the Book class
-        Book example = new Book("The Da Vinci Code");
-        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+        Book example = new Book("The Lord of the Rings");
+        System.out.println("Title (should be The Lord of the Rings): " + example.getTitle());
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
         example.rented();
         System.out.println("Borrowed? (should be true): " + example.isBorrowed());
