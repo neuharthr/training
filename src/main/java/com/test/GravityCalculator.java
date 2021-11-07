fe749a6e671adf4f12a6e81ae276dc5db260ab5c
neuharthr <neuharthr@gmail.com>
2021-11-07 23:00:05 +0000

    Updated current code

--- src/main/java/com/test/GravityCalculator.java
+++ src/main/java/com/test/GravityCalculator.java
@@ -7,6 +7,16 @@ class GravityCalculator {
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
+
+
+        //Compute the position of a falling object:
+        //   x(t) = 0.5 * (at)^2 + v(t) + x
+        //
+        //  a = acceleration = -9.81 m/s
+        //  t = falling time (in seconds)
+        //  v = initial velocity
+        //  x = initial position
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
